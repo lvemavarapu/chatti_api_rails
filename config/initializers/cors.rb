5078aa6114ba18cd769e6cb056f3fcd16f38d2ca
jairoAussie <jairoaussie@gmail.com>
2021-07-02 08:59:45 +1000

    local cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://clever-kirch-b35626.netlify.app/'
+    origins 'http://localhost:8000'
 
     resource '*',
       headers: :any,
