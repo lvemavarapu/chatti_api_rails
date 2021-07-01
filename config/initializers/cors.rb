20fcde029798f0f13149bb68c5fd87fda324b0fb
jairoAussie <jairoaussie@gmail.com>
2021-07-01 18:07:55 +1000

    change cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://clever-kirch-b35626.netlify.app/'
 
     resource '*',
       headers: :any,
