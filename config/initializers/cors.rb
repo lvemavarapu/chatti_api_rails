f74384f3cfbfcfa60706cfe97395b9dda9398de6
jairoAussie <jairoaussie@gmail.com>
2021-07-02 14:08:18 +1000

    change origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://silly-davinci-cb3c55.netlify.app'
 
     resource '*',
       headers: :any,
